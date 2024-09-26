c6e60e9e3d360a5540fd0cad535ba9de3500aa91
Vítor Emanuel Queiroz Ferreira <vitoremanuelqf@gmail.com>
2024-09-26 00:30:19 -0300

    Configuração na validação do email ao criar uma nova conta.

--- src/app/sign-up/sign-up-schema.ts
+++ src/app/sign-up/sign-up-schema.ts
@@ -1,11 +1,14 @@
 import { z } from 'zod'
 
+const regex = /^[a-zA-Z0-9._%+-]+@.*ifsp.*\.(edu\.com|edu\.br)$/
+
 export const signUpSchema = z
   .object({
     email: z
       .string()
       .min(1, 'O email é obrigatório.')
-      .email('Formato de email inválido.'),
+      .email('Formato de email inválido.')
+      .regex(regex, 'Por favor, utilize um e-mail institucional do IFSP.'),
     password: z
       .string()
       .min(1, 'A senha é obrigatória.')
